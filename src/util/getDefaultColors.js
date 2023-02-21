634e02a02cfcc649b3057a83dc2c78a43739c210
Giovanni Pires <giovannipds@gmail.com>
2023-02-21 16:55:58 -0300

    create utils to separate logic

---
+++ src/util/getDefaultColors.js
@@ -0,0 +1,14 @@
+module.exports = function getDefaultColors() {
+  return [
+    "#333745",
+    "#e63462",
+    "#fe5f55",
+    "#c7efcf",
+    "#eef5db",
+    "#00b4a6",
+    "#007db6",
+    "#ffe972",
+    "#9c7671",
+    "#0c192b",
+  ];
+};
